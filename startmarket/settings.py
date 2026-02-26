a6d3538f6fa0f68a1d86153555b2f6e3f88a28f2
atul <atulg0736@gmail.com>
2026-02-27 01:31:04 +0530

    maybe fixed

--- startmarket/settings.py
+++ startmarket/settings.py
@@ -72,9 +72,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'static/media/')
+MEDIA_URL="/media/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
